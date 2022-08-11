a283f94476855ae3e151f528734522f69ecd8bc6
Dominik <46494682+ludzik940@users.noreply.github.com>
2022-08-11 13:54:59 +0200

    Filtering portfolio items work on PC now.

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+window.onload = (event) => {
+    const filterItems = document.querySelectorAll('.portfolio--filter__item');
+    const portfolioItem = document.querySelectorAll('.portfolio-box');
+    const portfolioGrid = document.querySelector('.portfolio-change');
+
+
+
+    filterItems.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            let dataId = e.target.getAttribute('data-id');
+
+
+            // check if data-id is equal to ALL if it's true it set portfolio template into grid, if not it change template into FlexBox
+            if(dataId === 'all') {
+                portfolioGrid.classList.remove('portfolio--flex');
+                portfolioGrid.classList.add('portfolio--grid');
+            } else {
+                portfolioGrid.classList.add('portfolio--flex');
+                portfolioGrid.classList.remove('portfolio--grid');
+            }
+
+
+            // Set active class on button which is already clicked
+            for (let i = 0; i < filterItems.length; i++) {
+                filterItems[i].addEventListener("click", function() {
+                    let activeNow = document.getElementsByClassName('active');
+
+
+                  activeNow[0].className = activeNow[0].className.replace(" active", "");
+                  this.className += " active";
+                });
+              }
+
+
+            //   Check if photo got class name equal to checked data-id.
+           portfolioItem.forEach((photo) => {
+                if(!photo.classList.contains(dataId)) {
+                    photo.classList.add('hidden');
+                    photo.classList.remove(`photo_${photo.id}`);
+                } else {
+                    photo.classList.remove('hidden');
+                    photo.classList.add(`photo_${photo.id}`);
+                }
+            })
+
+        
+        })
+    })
+
+  };
